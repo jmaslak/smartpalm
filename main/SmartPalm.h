f373d6670f62c79610694f31f2482e4f8844def8
we7u <>
2005-11-04 20:32:38 +0000

    Adding a default baud rate definition.

--- main/SmartPalm.h
+++ main/SmartPalm.h
@@ -48,6 +48,7 @@
 #define DEFAULT_FAST_BEACON_RATE (180)
 #define DEFAULT_STOP_BEACON_RATE (600)
 #define DEFAULT_KISS_MODE        (0)
+#define DEFAULT_BAUD_RATE        (9600)
 
 // Aren't you glad that PI != 4?
 #define PI (3.14159265359)
